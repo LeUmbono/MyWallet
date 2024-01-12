5b0f545b6d0a77c02a96065f2da81058a6af5210
Russell Umboh <theumbono@gmail.com>
2024-01-12 15:29:20 -0800

    Initial Commit

---
+++ MyWallet/MyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWalletApp.swift
+//  MyWallet
+//
+//  Created by Russell Umboh on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
